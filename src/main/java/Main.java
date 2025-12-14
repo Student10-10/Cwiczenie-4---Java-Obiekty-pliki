bb977974f17e8db1fbdb5422dd92c5bb9e0599aa
Student10-10 <laborkastudent@gmail.com>
2025-12-14 18:12:52 +0000

    Commit4_3 - dodawanie nowego studenta z innymi danymi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,13 +11,12 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.Collection;
 
-
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
       Scanner sc = new Scanner(System.in);
-      System.out.println("1 - Dodaj studenta");
+System.out.println("1 - Dodaj studenta");
       System.out.println("2 - Wypisz wszystkich studentów");
       System.out.print("Wybierz opcję: ");
       int option = sc.nextInt();
@@ -25,12 +24,15 @@ class Main {
       if(option == 1) {
         System.out.print("Podaj imię: ");
         String name = sc.nextLine();
+        System.out.print("Podaj nazwisko: ");
+        String surname = sc.nextLine();
         System.out.print("Podaj wiek: ");
         int age = sc.nextInt();
-        s.addStudent(new Student(name, age));
+        s.addStudent(new Student(name, surname, age));
+        System.out.println("Dodano studenta: " + name + " " + surname + " " + age);
       }
       else if(option == 2) {
-        var students = s.getStudents();
+        Collection<Student> students = s.getStudents();
         for(Student current : students) {
           System.out.println(current.ToString());
         }
@@ -38,7 +40,6 @@ class Main {
       else {
         System.out.println("Nieprawidłowa opcja");
       }
-
       sc.close();
     } catch (IOException e) {
       System.out.println("Błąd operacji na pliku");
